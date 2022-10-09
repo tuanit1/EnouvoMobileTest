1af24955dc0f8b5783550d367490ccbddbfd55e8
Do Thanh Tuan <81170150+tuanit1@users.noreply.github.com>
2022-10-09 22:38:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/enouvomobiletest/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.enouvomobiletest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.enouvomobiletest.R
+import androidx.lifecycle.viewModelScope
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
