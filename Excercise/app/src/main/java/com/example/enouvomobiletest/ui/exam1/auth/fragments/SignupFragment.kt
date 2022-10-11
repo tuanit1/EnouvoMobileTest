5c736908b6a77ceae09ab1af504d3d3470a84b22
Do Thanh Tuan <81170150+tuanit1@users.noreply.github.com>
2022-10-12 00:47:26 +0700

    Create fragment_signup.xml

--- Excercise/app/src/main/java/com/example/enouvomobiletest/ui/exam1/auth/fragments/SignupFragment.kt
+++ Excercise/app/src/main/java/com/example/enouvomobiletest/ui/exam1/auth/fragments/SignupFragment.kt
@@ -16,7 +16,7 @@ class SignupFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         binding = FragmentSignupBinding.inflate(inflater, container, false)
 
