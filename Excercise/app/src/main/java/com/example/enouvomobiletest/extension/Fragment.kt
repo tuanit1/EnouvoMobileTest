e546cfc815bfd9aeeb2a4afc34a6d64125f3ced0
Do Thanh Tuan <81170150+tuanit1@users.noreply.github.com>
2022-10-16 17:28:48 +0700

    Add fragment transitions

--- Excercise/app/src/main/java/com/example/enouvomobiletest/extension/Fragment.kt
+++ Excercise/app/src/main/java/com/example/enouvomobiletest/extension/Fragment.kt
@@ -2,15 +2,24 @@ package com.example.enouvomobiletest.extension
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import com.example.enouvomobiletest.R
 
 fun Fragment.replaceFragment(
     containerId: Int,
     fragment: Fragment,
     addToBackStack: Boolean = false,
     tag: String
-){
+) {
     if (childFragmentManager.findFragmentByTag(tag) == null) {
         childFragmentManager.commit {
+
+            if(tag == getString(R.string.tweet)){
+                setCustomAnimations(R.anim.slide_right_in, R.anim.slide_right_out)
+            }else{
+                setCustomAnimations(R.anim.slide_left_in, R.anim.slide_left_out)
+            }
+
+
             replace(containerId, fragment, tag)
             if (addToBackStack) {
                 addToBackStack(tag)
@@ -24,7 +33,7 @@ fun Fragment.addFragment(
     fragment: Fragment,
     addToBackStack: Boolean = false,
     tag: String
-){
+) {
     if (childFragmentManager.findFragmentByTag(tag) == null) {
         childFragmentManager.commit {
             add(containerId, fragment, tag)
